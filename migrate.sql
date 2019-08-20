f4cf4dd509a300afc1b2156a720fa8faa18ee58f
Cameron Kingsbury <camsbury7@gmail.com>
2019-08-20 00:03:46 -0400

    feat: add items to data model
    Add items (name and description) as well as a relation to queues,
    these relations also encode priority by pointing at their parent
    item in the queue.

--- migrate.sql
+++ migrate.sql
@@ -6,9 +6,23 @@ CREATE TABLE IF NOT EXISTS queues
   description TEXT
 );
 
-INSERT INTO queues
+INSERT OR IGNORE INTO queues
 VALUES (
   'd3f8e89c-8994-5d24-ff33-5610abfaf294',
   'global',
   'All items enter this queue for a global prioritization.'
+);
+
+CREATE TABLE IF NOT EXISTS items
+( id VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  description TEXT
+);
+
+/* Not only relate items to queue, but set the stage for
+the priority queue impl */
+CREATE TABLE IF NOT EXISTS items_queues
+( item_id VARCHAR(36) NOT NULL REFERENCES items(id),
+  queue_id VARCHAR(36) NOT NULL REFERENCES queue(id),
+  parent VARCHAR(36) REFERENCES items(id)
 );
